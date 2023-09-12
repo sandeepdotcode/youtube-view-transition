6cddac451d38104173c9893b2dbbac3c6433e420
Sandeep K K <sandeepdotcode@gmail.com>
2023-09-12 21:21:32 +0530

    Fix sidebar scroll with feed & Enable node SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-
-import svelte from "@astrojs/svelte";
+import tailwind from '@astrojs/tailwind';
+import nodejs from '@astrojs/node';
+import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
+  integrations: [tailwind(), svelte()],
+  output: 'server',
+  adapter: nodejs({
+    mode: 'middleware'
+  }),
 });
