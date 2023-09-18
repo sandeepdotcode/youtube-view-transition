8725e170284d48a5f053d6ed52a61c9c884ad28f
Sandeep K K <sandeepdotcode@gmail.com>
2023-09-18 13:29:45 +0530

    Replace Node SSR adapter with Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import nodejs from '@astrojs/node';
 import svelte from '@astrojs/svelte';
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
-  output: 'server',
-  adapter: nodejs({
-    mode: 'middleware'
-  }),
+  output: "server",
+  adapter: vercel()
 });
